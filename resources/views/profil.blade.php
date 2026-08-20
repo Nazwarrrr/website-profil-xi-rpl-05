79834445a584e37369fb2d97c136b5ed9fa0b786
Annuraini09 <annurainisr04@gmail.com>
2026-08-20 09:32:50 +0700

    Menambahkan halaman profil kelas

---
+++ resources/views/profil.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Profil XI RPL</title>
+</head>
+<body>
+    <h1>XI Rekayasa Perangkat Lunak</h1>
+    <h2>SMKN 1 Garut</h2>
+    <p>Kami adalah siswa kelas XI RPL yang mempelajari pengembangan perangkat lunak.</p>
+    
+    <h3>Visi Kelas</h3>
+    <p>Menjadi siswa yang terampil, berkarakter, dan siap bersaing di dunia industri.</p>
+</body>
+</html>
